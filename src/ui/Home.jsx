a6138b9ab91f37a64054a8230ba89926a981efd0
kourosh <kourosh9bhl@gmail.com>
2024-01-11 13:01:30 +0330

    Ready for tailwind

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className=" bg-black text-center text-xl text-yellow-400">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
